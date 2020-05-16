306ce33bb1cae52c43c440218ff80563df4358e4
Akshay Ashok <aks28id@gmail.com>
2020-05-16 11:44:17 +0530

    dockerized nextjs

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,19 +1,18 @@
-import { Flex, Heading, Image } from '@chakra-ui/core'
- 
+import { Flex, Heading, Image } from "@chakra-ui/core";
+
 const Hero = () => {
   return (
     <Flex flexDirection="row" alignItems="center" justifyContent="center">
-    <Heading fontSize="3xl" fontWeight="bold" textAlign="left">
-      community market place for deep learning projects
-    </Heading>
-    <Image
-      height="28rem"
-      width="28rem"
-      src="https://res.cloudinary.com/supermodel/image/upload/v1589546581/hero_img_yxyeru.svg"
-    />
-  </Flex>
-  )
-}
+      <Heading fontSize="3xl" fontWeight="bold" textAlign="left">
+        community market place for deep learning projects
+      </Heading>
+      <Image
+        height="28rem"
+        width="28rem"
+        src="https://res.cloudinary.com/supermodel/image/upload/v1589546581/hero_img_yxyeru.svg"
+      />
+    </Flex>
+  );
+};
 
-export default Hero
-
+export default Hero;
